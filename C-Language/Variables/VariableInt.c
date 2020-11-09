556ddc3e0b3334b0ed4a0d4160455770f3b77a35
Wali Khan <72927228+Walikhan007@users.noreply.github.com>
2020-11-09 11:58:34 +0530

    Create VariableInt.c

---
+++ C-Language/Variables/VariableInt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    /*int variable declaration*/
+    int i;
+
+    /*Appointing a value*/
+    i = 1234;
+
+    /*Printing the value of 'i'*/
+    printf("The value of 'i' is : '%d' ", i);
+
+    return 0;
+}
