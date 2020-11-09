624a61cc19a810945a7aba29b091cc484ba9b301
Wali Khan <72927228+Walikhan007@users.noreply.github.com>
2020-11-09 15:52:42 +0530

    C program

---
+++ C-Language/Algebra/Addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    /*Variable declaraton*/
+    int num1, num2, sum;
+
+    /*Appointing value*/
+    num1 = 20;
+    num2 = 30;
+
+    /*Adding values*/
+    sum = num1 + num2;
+
+    /*Printing the answer*/
+    printf("The sum of %d and %d is : %d", num1, num2, sum);
+
+    return 0;
+}
