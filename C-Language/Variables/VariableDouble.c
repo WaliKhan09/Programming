d2f14bb086ca9e292f93859c31dc06a3bd5ac8d8
Wali Khan <72927228+Walikhan007@users.noreply.github.com>
2020-11-09 11:59:19 +0530

    Add files via upload

---
+++ C-Language/Variables/VariableDouble.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    /*double variable declaration*/
+    double d;
+
+    /*Appointing a value*/
+    d = 135.2468;
+
+    /*Printing the value of 'd'*/
+    printf("The value of 'd' is : '%e' ", d);
+
+    return 0;
+}
